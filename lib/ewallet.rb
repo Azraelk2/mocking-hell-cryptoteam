6244fee9abda9b87a9f6281c5d8dba9825eef732
illumitata <gothic8282@gmail.com>
2017-12-24 01:34:57 +0100

    ewallet name passed

---
+++ lib/ewallet.rb
@@ -0,0 +1,14 @@
+# E-wallet that belongs to user of service
+class Ewallet
+  attr_accessor :id, :user, :name
+
+  def initialize(id, user, name)
+    @id = id
+    @user = user
+    @name = name
+  end
+
+  def name
+    @name || "#{@user.full_name}'s e-wallet"
+  end
+end
--- spec/ewallet_spec.rb
+++ spec/ewallet_spec.rb
@@ -1,11 +1,11 @@
 require 'spec_helper'
+require_relative '../lib/ewallet'
 
 describe Ewallet do
   it "should be named after user if name isn't set" do
     full_name = random_name
-    user = mock_model User, full_name: full_name
-    ewallet = Ewallet.new
-    ewallet.user = user
-    ewallet.name.should == "#{full_name}'s e-wallet"
+    user = double("Some user", :full_name => full_name)
+    ewallet = Ewallet.new(1910, user, nil)
+    expect(ewallet.name) == "#{full_name}'s e-wallet"
   end
 end
